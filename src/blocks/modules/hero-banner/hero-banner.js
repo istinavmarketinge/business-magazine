701ff03c2cdb73b011dbbee9c5ef194cc4878647
Ruslan Garapov <ruslangarapov1994@gmail.com>
2023-07-04 12:21:26 +0300

    first commit

---
+++ src/blocks/modules/hero-banner/hero-banner.js
@@ -0,0 +1,13 @@
+modules.define('hero-banner', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
---
+++ src/blocks/modules/info-links/info-links.js
@@ -0,0 +1,13 @@
+modules.define('info-links', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
